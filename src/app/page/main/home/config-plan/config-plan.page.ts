6d850375d2e218d0bcebb41ee84ec08149db299a
WidadG <widad.gonzalez@gmail.com>
2024-08-31 20:52:42 -0400

    La fase menstrual ahora se guarda como un campo en la tabla de usuario en firebase y los datos son visibles en config-plan

--- src/app/page/main/home/config-plan/config-plan.page.ts
+++ src/app/page/main/home/config-plan/config-plan.page.ts
@@ -14,7 +14,8 @@ export class ConfigPlanPage implements OnInit {
     weight: null,
     fitnessLevel: '',
     goals: '',
-    inicioCiclo: ''
+    inicioCiclo: '',
+    faseMenstrual: ''
   };
 
   faseMenstrual: string = '';
@@ -22,23 +23,47 @@ export class ConfigPlanPage implements OnInit {
   constructor(private firebaseService: FirebaseService, private router: Router) {}
 
   ngOnInit() {
-    // Cálculo inicial de la fase menstrual
-    this.calcularFaseMenstrual();
+    this.loadPersonaData(); //Carga los datos ingresados por el usuario anteriormente
+    this.calcularFaseMenstrual(); // Cálculo inicial de la fase menstrual
   }
   
+  async loadPersonaData() {
+    const userId = (await this.firebaseService.getAuth().currentUser)?.uid;
+    console.log('userId:', userId); // Verificar si el userId es correcto
+    
+    if (userId) {
+      this.firebaseService.getDocument(`users/${userId}`).then((data) => {
+        console.log('Data:', data); // Verifica si los datos son correctos
+  
+        if (data) { // Verifica si los datos no son null o undefined
+          this.personalData = data as any;
+          this.calcularFaseMenstrual();
+        } else {
+          console.log("No se encontraron datos para el usuario."); // Si no se encuentran datos
+        }
+      }).catch((error) => {
+        console.error("Error al cargar los datos:", error); // Captura errores al recuperar datos
+      });
+    } else {
+      console.error("El usuario no está logueado."); // Verificar si el usuario está logueado
+    }
+  }
+  
+
   async savePersonalData() {
     const userId = (await this.firebaseService.getAuth().currentUser)?.uid;
     if (userId) {
+      // Recalcular la fase menstrual antes de guardar los datos
+      this.calcularFaseMenstrual();
+  
+      // Añadir la fase menstrual a los datos del usuario
+      this.personalData.faseMenstrual = this.faseMenstrual;
+  
       this.firebaseService.setDocument(`users/${userId}`, this.personalData).then(() => {
         console.log('Los datos se han subido exitosamente');
-        // Recalcular la fase menstrual
-        this.calcularFaseMenstrual();
-        // Redirigir al usuario a la página de entrenamiento
         this.router.navigate(['./entrenamiento'], { queryParams: { fase: this.faseMenstrual } });
-
-
       }).catch((error) => {
-        console.error('Error al subir los datos: ', error);
+        console.error('Error al subir los datos:', error);
       });
     } else {
       console.error('El usuario no está logueado');
@@ -51,11 +76,11 @@ export class ConfigPlanPage implements OnInit {
       const today = new Date();
       const dayDifference = Math.floor((today.getTime() - inicioCiclo.getTime()) / (1000 * 60 * 60 * 24));
       const cycleDay = dayDifference % 28;
-
+  
       if (cycleDay >= 0 && cycleDay <= 5) {
-        this.faseMenstrual = 'Fase Folicular A - Menstruación';
+        this.faseMenstrual = 'Fase Folicular A – Menstruación';
       } else if (cycleDay > 5 && cycleDay <= 14) {
-        this.faseMenstrual = 'Fase Folicular B - Post Menstruación';
+        this.faseMenstrual = 'Fase Folicular B – Post Menstruación';
       } else if (cycleDay > 14 && cycleDay <= 17) {
         this.faseMenstrual = 'Fase Ovulación';
       } else if (cycleDay > 17 && cycleDay <= 28) {
--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -22,3 +22,4 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
