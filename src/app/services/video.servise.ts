31787e3d924169f69abc97e08d6e74541c5bfa9e
WidadG <widad.gonzalez@gmail.com>
2024-09-10 16:53:14 -0300

    Adicion de campos de videoEjercicio

--- src/app/services/video.servise.ts
+++ src/app/services/video.servise.ts
@@ -3,17 +3,20 @@ import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { Observable } from "rxjs";
 
 export interface Video {
-  id?: string;
-  titulo: string;
-  url: string;
-  descripcion?: string;
-  musculo: string;
-  desplegarVideo?: boolean;
-  thumbnail?: string;
-  series: number;
-  reps: number;
-  sets?: Set[];
+  id?: string;             // ID del video
+  titulo: string;          // Título del video
+  url: string;             // URL del video
+  descripcion?: string;    // Descripción del video
+  musculo: string;         // Músculo que trabaja el video
+  thumbnail?: string;      // URL de la imagen en miniatura
+  grupo?: string;          // Grupo (opcional)
+  tren?: string;           // Tren (opcional)
+  series?: number;         // Número de series (opcional)
+  reps?: number;           // Número de repeticiones (opcional)
+  sets?: Set[];            // Sets realizados en el ejercicio (opcional)
+  desplegarVideo?: boolean; // Estado para desplegar o no el video (opcional)
 }
+
 export interface Set {
   anterior: string;
   kg: number;
@@ -27,23 +30,27 @@ export interface Set {
 
 
 export class VideoService {
-  private collectionName = 'videoEjercicio';
+  private collectionName = 'videoEjercicio'; // Nombre de la colección en Firebase
 
   constructor(private firestore: AngularFirestore) { }
 
+    // Obtener todos los videos
   getVideos(): Observable<Video[]> {
     return this.firestore.collection<Video>(this.collectionName).valueChanges({ idField: 'id' });
   }
 
+  // Añadir un nuevo video a la colección
   addVideo(video: Video): Promise<void> {
     const id = this.firestore.createId();
     return this.firestore.collection(this.collectionName).doc(id).set({ ...video, id });
   }
 
+  // Eliminar un video por su ID
   deleteVideo(id: string): Promise<void> {
     return this.firestore.collection(this.collectionName).doc(id).delete();
   }
 
+  // Actualizar un video por su ID
   updateVideo(id: string, video: Video): Promise<void> {
     return this.firestore.collection(this.collectionName).doc(id).update(video);
   }
